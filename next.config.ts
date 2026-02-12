c1d2f1164ffce89790a631bd338dbe34932c6161
Ben Merckx <ben@codeurs.be>
2026-02-12 16:43:12 +0100

    Update alinea

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
 import {withAlinea} from 'alinea/next'
 import type {NextConfig} from 'next'
 
-console.log(process.env)
-
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   typescript: {
     // We check types in plenty other places, no need to waste time here
     ignoreBuildErrors: true
-  },
-  eslint: {
-    ignoreDuringBuilds: true
   },
   async redirects() {
     return [
